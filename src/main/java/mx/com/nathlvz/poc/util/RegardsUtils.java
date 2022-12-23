3e151782ce1477fb6436d7d580fc01d75098f5b4
Benjamin Natanael Ocotzi Alvarez <benjamin.ocotzi@sngular.com>
2022-12-22 23:13:01 -0600

    feat: Adding say hello method in spanish translation

--- src/main/java/mx/com/nathlvz/poc/util/RegardsUtils.java
+++ src/main/java/mx/com/nathlvz/poc/util/RegardsUtils.java
@@ -2,7 +2,11 @@ package mx.com.nathlvz.poc.util;
 
 public class RegardsUtils {
 
-	public static void sayHello() {
+	public static void sayHelloEnglish() {
 		System.out.println("Hello, my name is Benjamin and I am check how cherry pick works!");
+	}
+
+	public static void sayHelloSpanish() {
+		System.out.println("Hola, mi nombre es Benjamin y yo estoy checando como cherry pick trabaja!");
 	}
 }
